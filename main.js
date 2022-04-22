b51d4e4fbf57dfdf910180ae1eba604e90c651e2
Ando1405 <104229348+Ando1405@users.noreply.github.com>
2022-04-22 15:32:36 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+ScrollReveal().reveal('.showcase');
+
+ScrollReveal().reveal('.articulo1',{delay: 500});
+ScrollReveal().reveal('.articulo2',{delay: 500});
+
+ScrollReveal().reveal('.articulo3',{delay: 500});
+ScrollReveal().reveal('.articulo4',{delay: 500});
+ScrollReveal().reveal('.articulo5',{delay: 500});
+ScrollReveal().reveal('.articulo6',{delay: 500});
+ScrollReveal().reveal('.articulo7',{delay: 500});
+ScrollReveal().reveal('.articulo8',{delay: 500});
+ScrollReveal().reveal('.articulo9',{delay: 500});
+ScrollReveal().reveal('.articulo10',{delay: 500});
+ScrollReveal().reveal('.articulo11',{delay: 500});
